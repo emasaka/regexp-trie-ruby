36c3ba583524b732e7924e4621e0f597afd185e5
emasaka <emasaka@gmail.com>
2016-01-20 18:29:03 +0900

    initial commit
    $ bndle gem regexp-trie

---
+++ lib/regexp/trie.rb
@@ -0,0 +1,7 @@
+require "regexp/trie/version"
+
+module Regexp
+  module Trie
+    # Your code goes here...
+  end
+end
---
+++ lib/regexp/trie/version.rb
@@ -0,0 +1,5 @@
+module Regexp
+  module Trie
+    VERSION = "0.1.0"
+  end
+end
