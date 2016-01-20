0b46a8bfecb094920e20c5f39af1cbdee9268076
emasaka <emasaka@gmail.com>
2016-01-20 19:22:13 +0900

    initial implementation

--- lib/regexp/trie.rb
+++ lib/regexp/trie.rb
@@ -1,7 +1,48 @@
-module Regexp
-  module Trie
+class Regexp
+  class Trie
     VERSION = "0.1.0"
 
-    # Your code goes here...
+    def initialize
+      @trie = {}
+    end
+
+    def add(str)
+      ref = @trie
+      str.each_char do |c|
+        ref[c] ||= {}
+        ref = ref[c]
+      end
+      ref[:end] = true
+      self
+    end
+
+    def _regexp(node)
+      return nil if node[:end]
+      alt = []
+      cc = []
+      q = false
+      node.keys.sort.each do |c|
+        qc = Regexp.quote(c)
+        if node[c]
+          recurse = _regexp(node[c])
+          if recurse
+            alt << (qc + recurse)
+          else
+            cc << qc
+          end
+        else
+          q = true
+        end
+      end
+      cconly = alt.empty?
+      !cc.empty? and alt << (cc.size == 1 ? cc[0] : '[' + cc.join('') + ']')
+      result = alt.size == 1 ? alt[0] : '(?:' + alt.join('|') + ')'
+      q and result = cconly ? "#{result}?" : "(?:#{result})?"
+      result
+    end
+
+    def to_str
+      _regexp(@trie)
+    end
   end
 end
