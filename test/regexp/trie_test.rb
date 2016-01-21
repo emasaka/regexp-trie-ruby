8e0a13d9794fef2e21d5b2807ead4d9f2815262a
emasaka <emasaka@gmail.com>
2016-01-21 12:12:29 +0900

    write basic test

--- test/regexp/trie_test.rb
+++ test/regexp/trie_test.rb
@@ -5,7 +5,16 @@ class Regexp::TrieTest < Minitest::Test
     refute_nil ::Regexp::Trie::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_match_3_strings
+    re = %w(who where when).
+         reduce(Regexp::Trie.new) {|r, v| r.add(v) }.
+         to_regexp
+
+    assert_match re, 'who', "#{re} matches 'who'"
+    assert_match re, 'where', "#{re} matches 'where'"
+    assert_match re, 'when', "#{re} matches 'when'"
+
+    assert !(re =~ 'which'), "#{re} doesn't matche 'which'"
+    assert !(re =~ 'wh'), "#{re} doesn't matche 'wh'"
   end
 end
